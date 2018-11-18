5b1246a1bebb67b40e02a44e107654be82810dd7
John Leonardo <jdleonardo5@icloud.com>
2018-11-18 02:51:57 -0800

    error checks

--- party/SignupStep1VC.swift
+++ party/SignupStep1VC.swift
@@ -35,11 +35,44 @@ class SignupStep1VC: UIViewController, UITextFieldDelegate {
                 self.performSegue(withIdentifier: "goToSignup2", sender: nil)
             } else {
                 //text field too long
+                self.presentError(message: "Name can't be over 25 characters long.")
             }
         } else {
             //text field is empty
+            self.presentError(message: "Name can't be empty.")
         }
         return true
+    }
+    
+    //helper function to present error to user (uses SwiftMessages)
+    func presentError(message: String) {
+        // Instantiate a message view from the provided card view layout. SwiftMessages searches for nib
+        // files in the main bundle first, so you can easily copy them into your project and make changes.
+        let view = MessageView.viewFromNib(layout: .centeredView)
+        
+        // Theme message elements with the warning style.
+        view.configureTheme(.error)
+        
+        // Add a drop shadow.
+        view.configureDropShadow()
+        
+        view.button?.isHidden = true
+        
+        // Set message title, body, and icon. Here, we're overriding the default warning
+        // image with an emoji character.
+        view.configureContent(title: "Oops", body: message, iconText: "🤔")
+        
+        // Increase the external margin around the card. In general, the effect of this setting
+        // depends on how the given layout is constrained to the layout margins.
+        view.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+        var config = SwiftMessages.Config()
+        
+        config.presentationStyle = .center
+        config.duration = .seconds(seconds: 2)
+        
+        // Show the message.
+        SwiftMessages.show(config: config, view: view)
     }
 
 }
--- party/SignupStep2VC.swift
+++ party/SignupStep2VC.swift
@@ -18,10 +18,65 @@ class SignupStep2VC: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailField.delegate = self
+        
         let nameChunk = name.components(separatedBy: " ")
-        name = nameChunk[0]
-        nameLbl.text = "Hi, \(name ?? "friend")😎"
+        nameLbl.text = "Hi, \(nameChunk[0])😎"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if emailField.text != "", let email = emailField.text {
+            if email.isValidEmail() {
+                
+            } else {
+                //invalid email
+                self.presentError(message: "Your email is invalid.")
+            }
+        } else {
+            //is empty
+            self.presentError(message: "Your email can't be empty.")
+        }
+        return true
+    }
+    
+    //helper function to present error to user (uses SwiftMessages)
+    func presentError(message: String) {
+        // Instantiate a message view from the provided card view layout. SwiftMessages searches for nib
+        // files in the main bundle first, so you can easily copy them into your project and make changes.
+        let view = MessageView.viewFromNib(layout: .centeredView)
+        
+        // Theme message elements with the warning style.
+        view.configureTheme(.error)
+        
+        // Add a drop shadow.
+        view.configureDropShadow()
+        
+        view.button?.isHidden = true
+        
+        // Set message title, body, and icon. Here, we're overriding the default warning
+        // image with an emoji character.
+        view.configureContent(title: "Oops", body: message, iconText: "🤔")
+        
+        // Increase the external margin around the card. In general, the effect of this setting
+        // depends on how the given layout is constrained to the layout margins.
+        view.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+        var config = SwiftMessages.Config()
+        
+        config.presentationStyle = .center
+        config.duration = .seconds(seconds: 2)
+        
+        // Show the message.
+        SwiftMessages.show(config: config, view: view)
     }
 
 
+}
+
+extension String {
+    func isValidEmail() -> Bool {
+        // here, `try!` will always succeed because the pattern is valid
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
+    }
 }
