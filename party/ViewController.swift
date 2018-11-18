d3aa1b2dc22316a7d9f37916c7346790618a7093
John Leonardo <jdleonardo5@icloud.com>
2018-11-17 20:45:52 -0800

    Initial Commit

---
+++ party/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  party
+//
+//  Created by John Leonardo on 11/17/18.
+//  Copyright © 2018 John Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
