9a0bec4bf0975f0b5dec619c69ab556fbbe37e33
John Leonardo <jdleonardo5@icloud.com>
2018-11-20 18:48:54 -0800

    compose text view ui complete

--- party/ComposeTextpostVC.swift
+++ party/ComposeTextpostVC.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ComposeTextpostVC: UIViewController {
+class ComposeTextpostVC: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var cardView: UIView!
-
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textCountLbl: UILabel!
+    @IBOutlet weak var constraintOutlet: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //to get keyboard height
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        //set text view delegate
+        textView.delegate = self
+        
+        //placeholder stuff
+        textView.text = "Start typing..."
+        textView.textColor = UIColor.lightGray
 
         //style card view
         //cardView.layer.masksToBounds = true
@@ -23,6 +41,55 @@ class ComposeTextpostVC: UIViewController {
     @IBAction func goBack(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        constraintOutlet.constant = 0
+        textView.layoutIfNeeded()
+        if textView.text.isEmpty {
+            self.textCountLbl.text = "0/400"
+            textView.text = "Start typing..."
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if self.textView.text.count <= 400 {
+            //green
+            self.textCountLbl.textColor = UIColor(red:0.30, green:0.69, blue:0.31, alpha:1.0)
+        } else {
+            //red
+            self.textCountLbl.textColor = UIColor(red:0.96, green:0.26, blue:0.21, alpha:1.0)
+        }
+        self.textCountLbl.text = "\(self.textView.text.count)/400"
+    }
+    
+    /* Updated for Swift 4 */
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        //resize bottom constraint to match keyboard height (to avoid text covering)
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            constraintOutlet.constant = keyboardHeight
+            textView.layoutIfNeeded()
+        }
+    }
+    
 }
 
 extension UIView {
