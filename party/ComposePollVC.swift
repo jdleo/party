44ec3a626c4ebe0339c192f644b5d3a65f047ae4
John Leonardo <jdleonardo5@icloud.com>
2018-11-23 11:05:59 -0800

    polling stuff

--- party/ComposePollVC.swift
+++ party/ComposePollVC.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
-class ComposePollVC: UIViewController {
+class ComposePollVC: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var choice1textField: UITextField!
+    @IBOutlet weak var choice2textField: UITextField!
+    @IBOutlet weak var choice3textField: UITextField!
+    @IBOutlet weak var choice4textField: UITextField!
+    @IBOutlet weak var lengthSegment: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //set delegates
+        titleTextField.delegate = self
+        choice1textField.delegate = self
+        choice2textField.delegate = self
+        choice3textField.delegate = self
+        choice4textField.delegate = self
+        
         //style card view
         //cardView.layer.masksToBounds = true
         cardView.roundCorners([.topRight, .topLeft], radius: 20)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == titleTextField {
+            titleTextField.resignFirstResponder()
+            choice1textField.becomeFirstResponder()
+        } else if textField == choice1textField {
+            choice1textField.resignFirstResponder()
+            choice2textField.becomeFirstResponder()
+        } else if textField == choice2textField {
+            choice2textField.resignFirstResponder()
+            choice3textField.becomeFirstResponder()
+        } else if textField == choice3textField {
+            choice3textField.resignFirstResponder()
+            choice4textField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
     }
     
     @IBAction func goBack(_ sender: Any) {
